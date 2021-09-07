69aa3091d303942d3708a9278f54c6001ea35d3c
Savya2006 <87526775+Savya2006@users.noreply.github.com>
2021-09-08 00:04:17 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,33 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.5,
+            friction :0.0,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image= loadImage("block.png")
+        this.visiblity = 225;
+        World.add(world, this.body);
+        
+      }
+     
+      display(){
+        console.log(this.body.speed);
+        var pos= this.body.position;
+        if(this.body.speed <2.6){
+          imageMode(CENTER);
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.visiblity = this.visiblity - 10;
+        tint(255,this.visiblity);
+        image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+        pop();
+      }
+    }
+}
+
